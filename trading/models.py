d9954c6441a91e1e67bdf93696a60f844c73e0f6
zipu <choi.yosep@gmail.com>
2019-10-24 14:41:01 +0800

    .

--- trading/models.py
+++ trading/models.py
@@ -37,18 +37,47 @@ class Instrument(models.Model):
         ('Meat', '육류')
     ]
 
-    name = models.CharField(max_length=64) #상품명
-    symbol = models.CharField(max_length=16) #상품코드
-    currency = models.CharField(max_length=16, choices=CURRENCIES, default='USD') #통화
-    exchange = models.CharField(max_length=16, choices=EXCHANGES) #거래소
-    market = models.CharField(max_length=16, choices=MARKETS) #시장구분
-    tickunit = models.FloatField() #틱 단위
-    tickprice = models.DecimalField(max_digits=5, decimal_places=2) #틱당 가격
-    margin = models.PositiveIntegerField() #증거금
-    opentime = models.TimeField() #장 시작시간(한국)
-    closetime = models.TimeField() #장 종료시간(한국)
-    decimal_length = models.SmallIntegerField() #소수점 자리수
+    name = models.CharField("상품명", max_length=64) #상품명
+    symbol = models.CharField("상품코드", max_length=16) #상품코드
+    currency = models.CharField("거래 통화", max_length=16, choices=CURRENCIES, default='USD') #통화
+    exchange = models.CharField("거래소", max_length=16, choices=EXCHANGES) #거래소
+    market = models.CharField("시장 구분", max_length=16, choices=MARKETS) #시장구분
+    tickunit = models.FloatField("호가 단위") #틱 단위
+    tickprice = models.DecimalField("호가당 가격", max_digits=5, decimal_places=2) #틱당 가격
+    margin = models.PositiveIntegerField("증거금") #증거금
+    opentime = models.TimeField("거래 시작시간") #장 시작시간(한국)
+    closetime = models.TimeField("거래 종료시간") #장 종료시간(한국)
+    decimal_length = models.SmallIntegerField("소수점 자리수") #소수점 자리수
 
     def __str__(self):
         return f"{self.name} ({self.symbol})"
 
+class Entry(models.Model):
+    """ 진입 내역 """
+    POSITIONS = [
+        (1, "Long"),
+        (-1, "Short")
+    ]
+    date = models.DateTimeField("진입 날짜")
+    instrument = models.ForeignKey(Instrument, verbose_name="상품명", on_delete=models.PROTECT)
+    position = models.SmallIntegerField("포지션")
+    quantity = models.SmallIntegerField("계약수")
+    price = models.FloatField("진입가")
+    stopprice = models.FloatField("청산 예정가")
+    risk = models.FloatField("매매리스크")
+
+    def save(self, *args, **kwargs):
+        self.risk = int((self.price - self.stopprice)*self.position/self.instrument.tickunit)\
+                      * self.instrument.tickprice * self.quantity
+        super(Entry, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.instrument.name}/{self.date}/{self.risk}"
+
+
+class Exit(models.Model):
+    """ 청산 내역 """
+    pass
+    
+    
+    
