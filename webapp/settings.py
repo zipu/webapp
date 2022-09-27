42da4571a78819b9becc9b00c1b41d5ba273a167
zipu <choi.yosep@gmail.com>
2022-09-27 11:48:38 +0800

    python 3.8 -> 3.9, all packaged updated including django

--- webapp/settings.py
+++ webapp/settings.py
@@ -32,6 +32,11 @@ def get_secret(setting, secrets=secrets):
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
+
+# Modifications after upgraded to django 4
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret('SECRET_KEY')
 
