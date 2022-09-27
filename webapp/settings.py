0a9134f8a9b9684610af7ba37115f9d328313b86
zipu <choi.yosep@gmail.com>
2022-09-27 13:28:48 +0800

    ?

--- webapp/settings.py
+++ webapp/settings.py
@@ -16,7 +16,7 @@ from google.oauth2 import service_account
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print(BASE_DIR)
 
 with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
     secrets = json.load(secrets_file)
