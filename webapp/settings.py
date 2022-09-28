34c7e86289277d8443e3c293fd5c3271fe963f3c
zipu <choi.yosep@gmail.com>
2022-09-28 16:06:31 +0800

    dd

--- webapp/settings.py
+++ webapp/settings.py
@@ -360,7 +360,7 @@ MEDIA_URL = f'https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/webapp/'
 #login
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/index'
-#LOGOUT_REDIRECT_URL = '/login'
+LOGOUT_REDIRECT_URL = '/'
 
 #version
 MATH_APP_VERSION = '1.23' 
