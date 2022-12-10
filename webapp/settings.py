6440c2ea276f3bb1e3b5763089dfd90d6cd021da
zipu <choi.yosep@gmail.com>
2022-12-10 15:17:18 +0800

    Gitignore issue fixed

--- webapp/settings.py
+++ webapp/settings.py
@@ -226,7 +226,7 @@ USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
