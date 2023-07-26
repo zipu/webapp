eeffc16e58f1fe772105386e6da0fce90b459377
zipu <choi.yosep@gmail.com>
2023-07-26 12:36:28 +0800

    .

--- tools/currencyrate.py
+++ tools/currencyrate.py
@@ -5,16 +5,18 @@ querystring = ",".join([f"FRX.KRW{currency}" for currency in currencies])
 url = f'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes={querystring}'
 response = requests.get(url)
 if response.ok:
-    filename = "currency.json"
-    if  os.path.isfile(filename):
-        jsonobj = json.load(open(filename))
+    filename = "currencyrate.json"
+    filepath = os.path.join(os.path.dirname(__file__),filename)
+    if  os.path.isfile(filepath):
+        jsonobj = json.load(open(filepath))
     else:
         jsonobj = {"USD":[], "EUR":[], "CNY":[], "JPY":[], "HDK":[]}
 
     for currency in response.json():
         jsonobj[currency['currencyCode']].append([currency['date'], currency['basePrice']])
 
-    with open(filename, mode='w+', encoding='utf-8') as f:
+    with open(filepath, mode='w+', encoding='utf-8') as f:
         json.dump(jsonobj, f)
     
     print("환율 갱신 완료")
+    print(os.path.join(os.path.dirname(__file__),filename))
