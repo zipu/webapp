1f62efb2e7ca000848b91ccab2e4ec456fceac7d
zipu <choi.yosep@gmail.com>
2024-09-12 14:57:56 +0800

    currency

--- tools/currencyrates.py
+++ tools/currencyrates.py
@@ -1,7 +1,30 @@
 import requests, os
 from csv import writer
+from currency_converter import CurrencyConverter
+from datetime import datetime
+
+
+c=CurrencyConverter()
+basedir = os.path.join(os.path.dirname(__file__), 'currencyrates')
 
 currencies = ["USD","EUR","CNY","JPY","HKD"]
+now = datetime.now()
+date = now.strftime('%Y%m%d')
+time = now.strftime('%H%M%S')
+for code in currencies:
+    rate = c.convert(1, code, 'KRW')
+    filename = f"{code}.csv"
+    filepath = os.path.join(basedir, filename)
+    with open(filepath, mode='a', encoding='utf-8', newline='') as f:
+            wobj = writer(f)
+            wobj.writerow([date, time, rate])
+            f.close()
+print("환율 갱신 완료")
+
+
+
+
+"""
 querystring = ",".join([f"FRX.KRW{currency}" for currency in currencies])
 url = f'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes={querystring}'
 response = requests.get(url)
@@ -20,3 +43,4 @@ if response.ok:
             f.close()
 
     print("환율 갱신 완료")
+"""
